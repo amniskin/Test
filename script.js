cc86c53b62bc40855c0f8c47ddf4b89e4900719a
amniskin <amniskin@Aarons-MacBook-Air.local>
2015-01-01 13:42:40 -0500

    nothing really
    ‘div’ to this

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
                   $('div').mouseenter(function(){
-                                      $('div').fadeTo('fast', 1);
+                                      $(this).fadeTo('fast', 1);
                                       });
                   $('div').mouseleave(function(){
-                                      $('div').fadeTo('fast', 0.5);
+                                      $(this).fadeTo('fast', 0.5);
                                       });
 });
